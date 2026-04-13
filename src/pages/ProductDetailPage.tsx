3a732675d052080e55105830c60955ed01b72768
symplyn-co <idontknowwwwww69@gmail.com>
2026-04-13 20:09:47 +0530

    fix: SPA routing issue on refresh and changed stuff

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -85,7 +85,7 @@ const ProductDetailPage = () => {
                 <div className="flex flex-wrap gap-4 mt-8">
                   <a
                     target="_blank"
-                    href={`https://wa.me/919427271597?text=${encodeURIComponent(
+                    href={`https://wa.me/919574735141?text=${encodeURIComponent(
   `Hi, I'm interested in:\n${product.name} (${product.code})\n${window.location.href}`
 )}`}
                     rel="noopener noreferrer"
